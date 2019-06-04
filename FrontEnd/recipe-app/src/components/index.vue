fa7a7a058679d64f5ab67f818f7a736fc18dcfea
michele byman <michelebyman@gmail.com>
2019-06-04 16:17:42 +0200

    table fixed

--- FrontEnd/recipe-app/src/components/index.vue
+++ FrontEnd/recipe-app/src/components/index.vue
@@ -1,25 +1,38 @@
 <template>
   <div class="wrapper">
-    <ul>
-      <li v-for="recipe in recipes" :key="recipe.id">
-        {{ recipe }}
-      </li>
-    </ul>
+    <div class="recipeWrapper">
+      <div>
+        <img src alt>
+      </div>
+
+      <table>
+        <tbody v-for="recipe in recipes" :key="recipe.id">
+          <th>{{recipe.title}}</th>
+          <tr>
+            <td v-for="ingredient in recipe.ingredients" :key="ingredient.id">
+              <tr v-for="key in ingredient" :key="key.id">
+                <td>{{key}}</td>
+              </tr>
+            </td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
-  name: 'Index',
+  name: "Index",
   props: {
     recipes: Array
   }
-}
+};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-  ul {
-    list-style: none;
-  }
+ul {
+  list-style: none;
+}
 </style>
