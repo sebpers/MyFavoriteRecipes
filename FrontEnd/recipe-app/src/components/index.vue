72e9f4bc26fefa5139432e5e09e3cc8d361d6b09
michele byman <michelebyman@gmail.com>
2019-06-05 09:14:13 +0200

    recipe iteration fixed and the schema

--- FrontEnd/recipe-app/src/components/index.vue
+++ FrontEnd/recipe-app/src/components/index.vue
@@ -1,38 +1,46 @@
 <template>
-  <div class="wrapper">
-    <div class="recipeWrapper">
-      <div>
-        <img src alt>
-      </div>
+    <div class="wrapper">
+        <div class="recipeWrapper">
+            <div>
+                <img src alt>
+            </div>
 
-      <table>
-        <tbody v-for="recipe in recipes" :key="recipe.id">
-          <th>{{recipe.title}}</th>
-          <tr>
-            <td v-for="ingredient in recipe.ingredients" :key="ingredient.id">
-              <tr v-for="key in ingredient" :key="key.id">
-                <td>{{key}}</td>
-              </tr>
-            </td>
-          </tr>
-        </tbody>
-      </table>
+            <!-- <table>
+                <tbody v-for="recipe in recipes" :key="recipe.id">
+                    <th>{{recipe.title}}</th>
+                    <tr>
+                        <td v-for="ingredient in recipe.ingredients" :key="ingredient.id">
+                            <tr v-for="key in ingredient" :key="key.id">
+                                <td>{{key}}</td>
+                            </tr>
+                        </td>
+                    </tr>
+                </tbody>
+            </table> -->
+              <table>
+                <tbody v-for="recipe in recipes" :key="recipe.id">
+                    <th>{{recipe.title}}</th>
+                    <tr v-for="ingredient in recipe.ingredients" :key="ingredient.id">
+                        <td v-for="key in ingredient" :key="key.id">{{key}}</td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
     </div>
-  </div>
 </template>
 
 <script>
 export default {
-  name: "Index",
-  props: {
-    recipes: Array
-  }
+    name: "Index",
+    props: {
+        recipes: Array
+    }
 };
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
 ul {
-  list-style: none;
+    list-style: none;
 }
 </style>
